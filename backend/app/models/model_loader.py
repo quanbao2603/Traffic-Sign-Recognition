362d2ab0d6872144f5bd4f198e042536f50c45f5
quanbao2603 <nguyenquanbao2632005@gmail.com>
2025-12-07 19:45:43 +0700

    update url for model

--- backend/app/models/model_loader.py
+++ backend/app/models/model_loader.py
@@ -1,12 +1,15 @@
 import os
 from ultralytics import YOLO
 
-# Lấy đường dẫn tuyệt đối đến thư mục app
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# BASE_DIR = thư mục gốc dự án traffic-sign-recognition/
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..", "..")
+)
 
-# Đường dẫn đến model
+# Đường dẫn tới model/best.pt
 MODEL_PATH = os.path.join(BASE_DIR, "model", "best.pt")
 
-print("🔍 Loading model from:", MODEL_PATH)
+print("📌 Loading YOLO model from:", MODEL_PATH)
 
+# Load model YOLO đã train
 model = YOLO(MODEL_PATH)
